581cb38ca843e3916885f81c1b9c27706e8ce32b
Adam Hartz <ahartz1@gmail.com>
2015-09-22 16:59:35 -0400

    Add separator phone_number

--- textminer/separator.py
+++ textminer/separator.py
@@ -2,22 +2,24 @@ import re
 import textminer.validator as v
 
 def words(s):
+    if not v.words(s):
+        return None
     ret = []
     word_list = re.split(r' ', s)
     for word in word_list:
         if v.words(word):
             ret.append(word)
-    if len(ret) == 0:
-        return None
-    else:
+    return ret
+
+def phone_number(s):
+    if v.phone_number(s):
+        ret = {}
+        p_num = re.sub(r'[^0-9]', '', s)
+        ret['area_code'] = p_num[:3]
+        ret['number'] = p_num[3:6] + '-' + p_num[6:]
         return ret
-
-def phone_numbers(s):
-    pass        
-
-
-
-
+    else:
+        return None
 
 
 
